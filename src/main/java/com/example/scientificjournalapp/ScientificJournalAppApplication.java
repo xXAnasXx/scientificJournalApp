f6a57e3efa565c97d1155441dcca27a4044d2023
xXAnasXx <anass-ab-@hotmail.com>
2022-12-09 01:50:20 -0500

    first commit

---
+++ src/main/java/com/example/scientificjournalapp/ScientificJournalAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.scientificjournalapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScientificJournalAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScientificJournalAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scientificjournalapp/ScientificJournalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scientificjournalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScientificJournalAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
